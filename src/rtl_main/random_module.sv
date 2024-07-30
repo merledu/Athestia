11725b9c2fa0dd399f98bb4ea8f910eb0f33cf38
masfiyan <asfiyanshivani0321@gmail.com>
2024-07-31 02:02:47 +0500

    random update

--- src/rtl_main/random_module.sv
+++ src/rtl_main/random_module.sv
@@ -10,23 +10,22 @@ module random_module (
     logic [31:0] seed;
 
     initial begin
-        // Use system call to get the current time (seconds since epoch)
         integer fd;
         integer seed_file;
         fd = $fopen("/dev/urandom", "r");
         seed_file = $fgetc(fd);
         $fclose(fd);
 
-        seed = seed_file ^ $time; // Combine with simulation time
-        $urandom(seed); // Set the seed for $urandom
-        $display("Seed: %0d", seed); // Display the seed for debugging purposes
+        seed = seed_file ^ $time; // xor
+        $urandom(seed);
+        $display("Seed: %0d", seed);
     end
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             rand_num <= 32'sd0;
         end else if (enable) begin
-            rand_num <= $urandom; // Generate random number
+            rand_num <= $urandom;
         end
     end
 

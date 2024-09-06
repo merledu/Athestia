de02ccce60dbbe4b6d3f8debbcb91a93e017181d
amir664 <amirjaved040@gmail.com>
2024-09-06 17:50:13 +0500

    SA test

--- src/test/state_array_test.cpp
+++ src/test/state_array_test.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
-#include "VAes_ctr.h"
+#include "Vstate_array.h"
 
 vluint64_t sim_time = 0;
 
-void tick(VAes_ctr* dut) {
+void tick(Vstate_array* dut) {
     dut->clk = !dut->clk;
     dut->eval();
     sim_time++;
@@ -13,12 +13,12 @@ void tick(VAes_ctr* dut) {
 
 int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
-    VAes_ctr* dut = new VAes_ctr;
+    Vstate_array* dut = new Vstate_array;
 
     Verilated::traceEverOn(true);
     VerilatedVcdC* m_trace = new VerilatedVcdC;
     dut->trace(m_trace, 99); 
-    m_trace->open("Aes_ctr.vcd");
+    m_trace->open("state_array.vcd");
     //
     dut->clk = 0;
     dut->reset = 0;

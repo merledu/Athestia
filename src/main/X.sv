4163f81c9987243c8503d9fd119ef4b57c8df0cb
amir664 <amirjaved040@gmail.com>
2024-09-10 15:48:30 +0500

    chi_update

--- src/main/X.sv
+++ src/main/X.sv
@@ -1,8 +1,9 @@
+`include "src/main/config_module.sv"
 module X(
     input logic clk,
     input logic reset,
-    input logic [63:0] A [4:0][4:0],
-    output logic [63:0] Ab [4:0][4:0]
+    input logic [`w-1:0] A [4:0][4:0],
+    output logic [`w-1:0] Ab [4:0][4:0]
 );
 
 // +
@@ -10,7 +11,7 @@ always_ff @(posedge clk or negedge reset) begin
     if (~reset) begin
         for (int i = 0; i < 5; i++) begin  
             for (int j = 0; j < 5; j++) begin 
-               for (int k = 0; k < 64; k++) begin 
+               for (int k = 0; k < `w; k++) begin 
                 Ab[i][j][k]= A[i][j][k] ^ ((A[(i+1)%5][j][k] ^ 1) * A[(i+2)%5][j][k]);
                                 //1             //0        
                  

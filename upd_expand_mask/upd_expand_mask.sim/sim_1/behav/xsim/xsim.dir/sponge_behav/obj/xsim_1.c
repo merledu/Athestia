01bdd85fe4e42ff9e87d81191dd0594d1457137e
M.Amir jawed <123206488+amir664@users.noreply.github.com>
2025-03-17 00:35:19 +0500

    expand_mask

---
+++ upd_expand_mask/upd_expand_mask.sim/sim_1/behav/xsim/xsim.dir/sponge_behav/obj/xsim_1.c
@@ -0,0 +1,161 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_2(char*, char *);
+extern void execute_3(char*, char *);
+extern void vlog_const_rhs_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+extern void execute_379(char*, char *);
+extern void execute_380(char*, char *);
+extern void execute_381(char*, char *);
+extern void execute_382(char*, char *);
+extern void execute_383(char*, char *);
+extern void execute_384(char*, char *);
+extern void execute_385(char*, char *);
+extern void execute_386(char*, char *);
+extern void execute_387(char*, char *);
+extern void execute_388(char*, char *);
+extern void execute_389(char*, char *);
+extern void execute_390(char*, char *);
+extern void execute_391(char*, char *);
+extern void execute_392(char*, char *);
+extern void execute_393(char*, char *);
+extern void execute_5(char*, char *);
+extern void execute_6(char*, char *);
+extern void execute_7(char*, char *);
+extern void execute_134(char*, char *);
+extern void execute_135(char*, char *);
+extern void execute_136(char*, char *);
+extern void execute_137(char*, char *);
+extern void execute_138(char*, char *);
+extern void execute_139(char*, char *);
+extern void execute_140(char*, char *);
+extern void execute_141(char*, char *);
+extern void execute_142(char*, char *);
+extern void execute_143(char*, char *);
+extern void execute_144(char*, char *);
+extern void execute_145(char*, char *);
+extern void execute_146(char*, char *);
+extern void execute_147(char*, char *);
+extern void execute_148(char*, char *);
+extern void execute_149(char*, char *);
+extern void execute_150(char*, char *);
+extern void execute_151(char*, char *);
+extern void execute_152(char*, char *);
+extern void execute_153(char*, char *);
+extern void execute_154(char*, char *);
+extern void execute_155(char*, char *);
+extern void execute_156(char*, char *);
+extern void execute_157(char*, char *);
+extern void execute_158(char*, char *);
+extern void execute_159(char*, char *);
+extern void execute_160(char*, char *);
+extern void execute_130(char*, char *);
+extern void execute_131(char*, char *);
+extern void execute_132(char*, char *);
+extern void execute_133(char*, char *);
+extern void execute_10(char*, char *);
+extern void execute_12(char*, char *);
+extern void execute_14(char*, char *);
+extern void execute_16(char*, char *);
+extern void execute_18(char*, char *);
+extern void execute_125(char*, char *);
+extern void execute_127(char*, char *);
+extern void execute_128(char*, char *);
+extern void execute_129(char*, char *);
+extern void execute_394(char*, char *);
+extern void execute_395(char*, char *);
+extern void execute_396(char*, char *);
+extern void execute_397(char*, char *);
+extern void execute_398(char*, char *);
+extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[67] = {(funcp)execute_2, (funcp)execute_3, (funcp)vlog_const_rhs_process_execute_0_fast_no_reg_no_agg, (funcp)execute_379, (funcp)execute_380, (funcp)execute_381, (funcp)execute_382, (funcp)execute_383, (funcp)execute_384, (funcp)execute_385, (funcp)execute_386, (funcp)execute_387, (funcp)execute_388, (funcp)execute_389, (funcp)execute_390, (funcp)execute_391, (funcp)execute_392, (funcp)execute_393, (funcp)execute_5, (funcp)execute_6, (funcp)execute_7, (funcp)execute_134, (funcp)execute_135, (funcp)execute_136, (funcp)execute_137, (funcp)execute_138, (funcp)execute_139, (funcp)execute_140, (funcp)execute_141, (funcp)execute_142, (funcp)execute_143, (funcp)execute_144, (funcp)execute_145, (funcp)execute_146, (funcp)execute_147, (funcp)execute_148, (funcp)execute_149, (funcp)execute_150, (funcp)execute_151, (funcp)execute_152, (funcp)execute_153, (funcp)execute_154, (funcp)execute_155, (funcp)execute_156, (funcp)execute_157, (funcp)execute_158, (funcp)execute_159, (funcp)execute_160, (funcp)execute_130, (funcp)execute_131, (funcp)execute_132, (funcp)execute_133, (funcp)execute_10, (funcp)execute_12, (funcp)execute_14, (funcp)execute_16, (funcp)execute_18, (funcp)execute_125, (funcp)execute_127, (funcp)execute_128, (funcp)execute_129, (funcp)execute_394, (funcp)execute_395, (funcp)execute_396, (funcp)execute_397, (funcp)execute_398, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 67;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/sponge_behav/xsim.reloc",  (void **)funcTab, 67);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/sponge_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+	iki_schedule_processes_at_time_zero(dp, "xsim.dir/sponge_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/sponge_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/sponge_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/sponge_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
